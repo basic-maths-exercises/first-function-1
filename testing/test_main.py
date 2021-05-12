b17fedac37d4d01f0145cb454f4ee51602ead022
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-12 11:09:53 +0100

    Added tests for exercise

---
+++ testing/test_main.py
@@ -0,0 +1,20 @@
+try:
+    from AutoFeedback.funcchecks import check_func 
+except:
+    import subprocess
+    import sys
+            
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "AutoFeedback"])
+    from AutoFeedback.funcchecks import check_func 
+           
+import unittest
+from main import *
+
+class UnitTests(unittest.TestCase) :
+    def test_variables(self) : 
+        inputs, variables = [], []
+        for i in range(20) :
+            a, b = np.random.uniform(0,1), np.random.uniform(0,1)
+            inputs.append((a,b,))
+            variables.append( a*b )
+        assert( check_func('multiply',inputs, variables ) )
